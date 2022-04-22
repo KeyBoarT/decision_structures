8b82926f188b9ddea0b908d76e57f62cb7050cde
Mehmet Sezer <mehmetszr05@gmail.com>
2022-04-23 01:56:11 +0300

    Decision_structures

---
+++ decision_structures/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace decision_structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Decision Structures (Karar Yapıları)
+            // if, else, elseif, ternary
+
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın !");
+            }
+
+            else if (time >= 18)
+            {
+                Console.WriteLine("İyi günler !");
+            }
+            else
+            {
+                Console.WriteLine("iyi geceler !");
+            }
+
+
+            //Ternary
+            string sonuc = time >= 6 && time < 11 ? "Günaydın !" : time <= 18 ? "İyi Günler !" : "İyi Geceler !";
+            Console.WriteLine(sonuc);
+
+            Console.ReadLine();
+        }
+    }
+}
